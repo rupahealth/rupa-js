71af56c27055647acd11b69867a9697cf3a6d8f0
Ben Davis <ben@bencdavis.com>
2022-05-09 16:10:08 -0400

    Setup eslint, typescript, and build scripts

---
+++ build.js
@@ -0,0 +1,19 @@
+const { build } = require("esbuild");
+const { dependencies, peerDependencies } = require("./package.json");
+
+const config = {
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
+};
+
+build({
+  ...config,
+  outfile: "dist/index.js",
+});
+
+build({
+  ...config,
+  outfile: "dist/index.esm.js",
+  format: "esm",
+});
